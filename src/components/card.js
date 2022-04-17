b7ef897a00f94c94056290a94c2fb799b365a4ed
YMilovets <y.milovets@yandex.ru>
2022-04-17 21:40:39 +0700

    ДобавлениÐAdd new exercises and change style of the main page

--- src/components/card.js
+++ src/components/card.js
@@ -7,9 +7,11 @@ export default class card extends Component {
             <div className="me-3 card-wrapper">
                 <div className="card">
                     <img className="card-img-top" src={item.img} alt="Здесь рыбы нет"></img>
-                    <div className="card-body">
-                        <h5 className="card-title">{item.title}</h5>
-                        <p className="card-text">{item.action}</p>
+                    <div className="card-body d-flex justify-content-between flex-column">
+                        <div className="card-content mb-3">
+                            <h5 className="card-title">{item.title}</h5>
+                            <p className="card-text">{item.action}</p>
+                        </div>
                         <Link to={"exercise/" + item.link} className="btn btn-primary">Выбрать</Link>
                     </div>
                 </div>

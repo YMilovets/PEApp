dbeef5b2bc420d02ef8765f4527b7eea6aa9740e
YMilovets <y.milovets@yandex.ru>
2024-04-03 14:58:39 +0700

    Added a Close icon component in the PEApp project

--- src/Components/Icons/index.ts
+++ src/Components/Icons/index.ts
@@ -1,8 +1,9 @@
 import BackwardIcon from './BackwardIcon';
+import CloseIcon from './CloseIcon';
 import ForwardIcon from './ForwardIcon';
 import MenuIcon from './MenuIcon';
 import SearchIcon from './SearchIcon';
 
 export {
-  MenuIcon, BackwardIcon, ForwardIcon, SearchIcon,
+  MenuIcon, BackwardIcon, ForwardIcon, SearchIcon, CloseIcon,
 };
